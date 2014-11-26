ad99d6ec3a7c4a21cc83d52d7375daa94a64226c
Steve Sharman <steve@mediasmiths.com>
2014-11-26 11:51:07 +0000

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sxm-clojure-ms/lein-template "1.0.1"
+(defproject sxm-clojure-ms/lein-template "1.0.2-SNAPSHOT"
   :description "SynergyXM clojure microservice skeleton template for Leiningen. Generates a SynergyXM Clojure HTTP REST service"
   :url "https://github.com/smsharman/sxm-clojure-ms"
   :eval-in-leiningen true
